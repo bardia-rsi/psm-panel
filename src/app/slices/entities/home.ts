ff98d44cd4c1a0435f18d7853dae397608bd6cf7
Bardia Rasouli <bardiarasouli@protonmail.com>
2023-09-17 11:27:33 +0330

    Add the home slice

---
+++ src/app/slices/entities/home.ts
@@ -0,0 +1,63 @@
+import type { StatePreferences } from "@/types/App/States";
+import type { Error } from "@/types/App/Error";
+import type { FullFilledAction } from "@/types/App/ThunkActions";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { changeOrderReducer, changeSortByReducer } from "@/app/common/caseReducers";
+import { setLoadingStatus, setFailedStatus } from "@/app/common/extraReducers";
+import * as entitiesSlice from "@/app/slices/entities";
+
+const set = createAsyncThunk<
+    boolean, undefined, { rejectValue: Error }
+>("entities/home/set", async (_, { dispatch, rejectWithValue }) => {
+    try {
+
+        await dispatch(entitiesSlice.favorites.set());
+        await dispatch(entitiesSlice.contacts.set());
+        await dispatch(entitiesSlice.logins.set());
+        await dispatch(entitiesSlice.paymentCards.set());
+        await dispatch(entitiesSlice.wifiPasswords.set());
+
+        return true;
+
+    } catch (e: any) {
+        return rejectWithValue({
+            code: Number(e.response.status) || 500,
+            message: e.response.statusText || "Internal Error",
+            occurrenceTime: Date.now()
+        });
+    }
+});
+
+const slice = createSlice({
+    name: "entities/home",
+    initialState: { status: "idle", errors: {}, sortBy: "createdAt", order: "asc" } as StatePreferences,
+    reducers: {
+        "sortBy/change": changeSortByReducer,
+        "order/change": changeOrderReducer,
+    },
+    extraReducers: builder => {
+
+        builder
+            .addCase(set.pending.type, setLoadingStatus<StatePreferences>)
+            .addCase(set.rejected.type, setFailedStatus<StatePreferences>)
+            .addCase(set.fulfilled.type, (state, action: FullFilledAction<boolean, undefined>) => {
+
+                if (action.payload) {
+                    state.status = "succeeded";
+                } else {
+                    state.status = "failed";
+                }
+
+            });
+
+    }
+});
+
+export { set };
+
+export const {
+    "order/change": changeOrder,
+    "sortBy/change": changeSortBy
+} = slice.actions;
+
+export default slice.reducer;
---
+++ src/app/slices/entities/index.ts
@@ -0,0 +1,17 @@
+import * as home from "@/app/slices/entities/home";
+import * as trash from "@/app/slices/entities/trash";
+import * as favorites from "@/app/slices/entities/favorites";
+import * as contacts from "@/app/slices/entities/contacts";
+import * as logins from "@/app/slices/entities/logins";
+import * as paymentCards from "@/app/slices/entities/paymentCards";
+import * as wifiPasswords from "@/app/slices/entities/wifiPasswords";
+
+export {
+    home,
+    trash,
+    favorites,
+    contacts,
+    logins,
+    paymentCards,
+    wifiPasswords
+}
