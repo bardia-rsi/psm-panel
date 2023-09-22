aef2b118d813a3e878bef77f1746ef474007de4c
Bardia Rasouli <bardiarasouli@protonmail.com>
2023-09-22 23:18:01 +0330

    Add the loader for the 'items' section of the 'items' page

--- src/pages/Items/Loader.tsx
+++ src/pages/Items/Loader.tsx
@@ -13,6 +13,17 @@ const Loader: FC = (): ReactElement => {
                 <Skeleton h={2.25} />
                 <Skeleton h={2.25} w={4.25} />
             </div>
+            {
+                Array.from(Array(Math.floor((window.innerHeight - 68) / 96))).map((_, index) => (
+                    <div key={index} className="flex gap-x-4 p-4">
+                        <Skeleton h={3.5} w={3.5} />
+                        <div className="flex flex-col flex-1 gap-y-2 py-2">
+                            <Skeleton h={1.25} w="25%" />
+                            <Skeleton h={1.25} w="65%" />
+                        </div>
+                    </div>
+                ))
+            }
         </>
     );
 
